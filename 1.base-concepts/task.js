f633b4d24962bfc07c3f49503645b1dc3b940ae8
Nastikb <paps2703@yandex.ru>
2022-05-22 14:15:30 +0300

    update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,22 @@
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   // код для задачи №1 писать здесь
+  
+  if (a === 0) {
+    console.log(arr);
+    return arr;
+  };
+  
+  const d = b ** 2 - 4 * a * c;
+  console.log(`D = ${d}`);
+  
+  if (d === 0) {
+    arr.push((-b) / (2 * a));
+  } else if (d > 0) {
+    arr.push(((-b + Math.sqrt(d)) / (2 * a)));
+    arr.push(((-b - Math.sqrt(d)) / (2 * a)));
+  };
+  console.log(arr);
   return arr; // array
 }
 
