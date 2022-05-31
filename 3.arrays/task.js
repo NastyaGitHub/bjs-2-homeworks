1200b10ff9c4683a48aee3fa68fab7167ccfaa88
Nastikb <paps2703@yandex.ru>
2022-05-31 22:50:40 +0300

    Third task, update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,22 @@
 function compareArrays(arr1, arr2) {
   let result;
-
+  if (arr1.length === arr2.length) {
+    result = arr1.every((index) => {
+      if (arr1[index] === arr2[index]) {
+        return true;
+      }
+      return false;
+    });
+  } else {
+    return false;
+  }
   // Ваш код
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
   // Ваш код
-
+  let resultArr = arr.filter((arr) => arr >= 0).filter((arr) => arr % 3 === 0).map((arr) => arr * 10);
   return resultArr; // array
 }
